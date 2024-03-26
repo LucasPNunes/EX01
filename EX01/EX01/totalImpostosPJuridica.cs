436560d1e7901d65b0c030fadf90a6fd83d87397
Lucas Pimentel Nunes <111577567+LucasPNunes@users.noreply.github.com>
2024-03-26 17:03:51 -0300

    Add files via upload

---
+++ EX01/EX01/totalImpostosPJuridica.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX01
+{
+    internal class totalImpostosPJuridica
+    {
+        public double total { get; set; }
+    }
+}
